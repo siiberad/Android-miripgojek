f537b5782a6e808fb75944bf14f94ad74c04bea2
Tedy Ir <siiberad@gmail.com>
2019-05-09 16:27:35 +0700

    asu

--- app/src/main/java/com/siiberad/miripgojek/fragment/AccountFragment.java
+++ app/src/main/java/com/siiberad/miripgojek/fragment/AccountFragment.java
@@ -37,6 +37,7 @@ public class AccountFragment extends Fragment{
         textView7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Intent intent = new Intent(getActivity(), MyVoucherActivity.class);
                 getActivity().startActivity(intent);
             }
